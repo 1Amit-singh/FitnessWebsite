bb2e08b2c61e8efb9fff9649a382f8f2407eaba7
Amit Singh <iamitsinghthakur123@gmail.com>
2023-09-07 17:44:49 +0530

    Made some changes to carousel

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const counter = document.querySelector(".counter");
+let buttons = document.querySelector(".nav-btns")
+let singleButton = document.querySelectorAll(".nav-btn")
+
+buttons.addEventListener("click", (event)=> {
+    singleButton.forEach((val) => {
+        if (val.classList[1] === "active"){
+            val.classList.remove("active");
+        }
+    })
+    if(event.target.classList[0] !== "nav-btns"){
+        event.target.classList.add("active")
+    }
+})
 
 
 let speed = 500;
